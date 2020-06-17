ed1a136a1199c25df0d9b26f504cc7686fdc386f
JennH <marlowmayfield@hotmail.com>
2020-06-16 18:06:02 -0700

    modified nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,14 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'butterfly',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'A butterfly site for the entertainment and education of the butterfly enthusiast and lover of natural beauty.' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'https://www.si.edu/spotlight/buginfo/butterfly' }
     ]
   },
   /*
